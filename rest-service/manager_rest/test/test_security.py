9e1afdc9d2e715fff98b90e6998edf96b4a98298
Noa Kuperberg <noakup@gmail.com>
2015-03-22 16:20:56 +0200

    added more unit tests to check invalid creds

--- rest-service/manager_rest/test/test_security.py
+++ rest-service/manager_rest/test/test_security.py
@@ -1,4 +1,5 @@
 from base_test import BaseServerTestCase
+from cloudify_rest_client.exceptions import CloudifyClientError
 
 
 class SecurityTestCase(BaseServerTestCase):
@@ -9,4 +10,31 @@ class SecurityTestCase(BaseServerTestCase):
 
     def test_secured_client(self):
         client = self.create_client(user='user1', password='pass1')
-        client.deployments.list()
+        parameters = {'param1': 'val1', 'param2': 'val2'}
+        execution = client.executions.start(11,
+                                            'install',
+                                            parameters,
+                                            allow_custom_parameters=True)
+        client.executions.list()
+
+    def test_wrong_credentials(self):
+        client = self.create_client(user='user1', password='pass2')
+        self.assertRaises(CloudifyClientError, client.deployments.list)
+
+    def test_missing_credentials(self):
+        client = self.create_client()
+        self.assertRaises(CloudifyClientError, client.deployments.list)
+
+    def test_missing_user(self):
+        client = self.create_client(password='pass1')
+        self.assertRaises(CloudifyClientError, client.deployments.list)
+
+    def test_missing_password(self):
+        client = self.create_client(user='user1')
+        self.assertRaises(CloudifyClientError, client.deployments.list)
+
+    '''
+    def tearDown(self):
+        self._secured = True
+        super(SecurityTestCase, self).tearDown()
+    '''
